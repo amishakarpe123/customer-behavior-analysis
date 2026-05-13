7b63e126d859d697367d91b861cc05888ed9d132
amishakarpe123 <amishakarpe123@gmail.com>
2026-05-13 18:24:02 +0530

    Add files via upload

---
+++ E1DB.sql
@@ -0,0 +1,23 @@
+🟢 Beginner Level
+1️⃣ Show all records from the table.
+2️⃣ Show only FirstName, LastName and Salary.
+3️⃣ Show employees who live in 'Pune'.
+4️⃣ Show employees whose Salary is greater than 35,000.
+5️⃣ Show employees whose Age is between 25 and 30.
+6️⃣ Show employees from 'IT' department.
+7️⃣ Show employees where IsActive = 1.
+🟡 Beginner → Intermediate
+8️⃣ Show employees sorted by Salary (highest first).
+9️⃣ Show top 5 highest paid employees.
+🔟 Count total number of employees.
+1️⃣1️⃣ Find maximum salary.
+1️⃣2️⃣ Find minimum age.
+1️⃣3️⃣ Find average salary.
+1️⃣4️⃣ Count number of employees in each Department.
+🔵 Intermediate Level
+1️⃣5️⃣ Show total salary paid per Department.
+1️⃣6️⃣ Show departments having more than 3 employees.
+1️⃣7️⃣ Show employees whose name starts with 'A'.
+1️⃣8️⃣ Show employees who joined after '2024-01-10'.
+1️⃣9️⃣ Update salary by 10% for employees in IT department.
+2️⃣0️⃣ Delete employees where IsActive = 0.
